65da5cc3ae743e2659e298a4f8dd6a36f4e395ac
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-09-02 15:16:05 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1597,7 +1597,7 @@ function update_stats(){
 		
 		//if it's strong, make it waning
 		
-		if (el_style == "20px solid rgb(152, 176, 181)") 
+		if (el_style == "'20px solid " +color+"'") 
 			{
 			document.getElementById(element).style.border = "20px dashed rgb(152, 176, 181)";
 			
@@ -1616,7 +1616,7 @@ function update_stats(){
 		else if (el_style =="20px solid rgb(255, 255, 255)" && is_it_locked == 'false')
 			{
 			
-			document.getElementById(element).style.border = "20px solid rgb(152, 176, 181)";
+			document.getElementById(element).style.border = "'20px solid " +color+"'";
 			}
 	
 		//document.getElementById(element).style.border = "thick solid #0000FF"; && is_it_locked
