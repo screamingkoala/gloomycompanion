ee6473619855f4232f4c97db7c95e504e6cb4a3e
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-09-11 13:26:39 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1399,7 +1399,7 @@ function init() {
 		JSON_string ='{ name: "PC ' + document.getElementById("player_name_one").value + '", class: "PC", level: "' + document.getElementById("player_level_one").value+'"}';
 		//selected_deck_names[selected_deck_names.length] = JSON.parse(JSON_string);
 		console.log(JSON_string);
-		console.log(JSON.parse(JSON_string));
+		console.log(JSON.parse(JSON_string).replace(/\\"/g, '"'));
 		selected_deck_names[selected_deck_names.length] = JSON.stringify(JSON.parse("'"+ JSON_string+ "'"));
 				
 	}
