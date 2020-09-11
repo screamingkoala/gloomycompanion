8e7ed2c6a93049ba7b764afbf1722bc5fef256fd
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-09-11 10:57:33 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1389,7 +1389,7 @@ function init() {
 	if (document.querySelector('#player_check_one:checked') !== null && document.getElementById("player_name_one").value != "" && document.getElementById("player_level_one").value != ""){
 		//selected_deck_names[selected_deck_names.length] = "{name: \"PC " + document.getElementById("player_name_one").value, class: \"PC\", level: "+ document.getElementById("player_level_one").value+"};
 		console.log("{name: \"PC " + document.getElementById("player_name_one").value + "\", class: \"PC\", level: \""+ document.getElementById("player_level_one").value+"\"}");		
-		string = "{name: \"PC " + document.getElementById("player_name_one").value + "\", class: \"PC\", level: \""+ document.getElementById("player_level_one").value+"\"}");		
+		string = "{name: \"PC " + document.getElementById("player_name_one").value + "\", class: \"PC\", level: \""+ document.getElementById("player_level_one").value+"\"}";		
 		console.log(string);
 	}
 	
