4a0313ec9418c237d3fbd745181fbdabd68f28af
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-09-11 14:00:45 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1389,11 +1389,9 @@ function init() {
 	    
 	 
 	if (document.querySelector('#player_check_one:checked') !== null && document.getElementById("player_name_one").value != "" && document.getElementById("player_level_one").value != ""){
-		JSON_string ={ "name":"PC Lee", "class":"PC", "level":"1" };
-		
-		console.log(JSON_string);
-		console.log(JSON.parse(JSON_string));
-		selected_deck_names[selected_deck_names.length] = JSON.parse(JSON_string);
+		//JSON_string ={ "name":"PC Lee", "class":"PC", "level":"1" };
+			
+		//selected_deck_names[selected_deck_names.length] = JSON.parse(JSON_string);
 		
 		//selected_deck_names[selected_deck_names.length] = "{name: \"PC " + document.getElementById("player_name_one").value, class: \"PC\", level: "+ document.getElementById("player_level_one").value+"};
 		console.log("{ name: \"PC " + document.getElementById("player_name_one").value + "\", class: \"PC\", level: \""+ document.getElementById("player_level_one").value+"\"}");		
@@ -1401,8 +1399,8 @@ function init() {
 		JSON_string ='{ "name": "PC ' + document.getElementById("player_name_one").value + '", "class": "PC", "level": "' + document.getElementById("player_level_one").value+'"}';
 		//selected_deck_names[selected_deck_names.length] = JSON.parse(JSON_string);
 		console.log(JSON_string);
-		console.log(JSON.parse(JSON_string).replace(/\\"/g, '"'));
-		selected_deck_names[selected_deck_names.length] = JSON.stringify(JSON.parse("'"+ JSON_string+ "'"));
+		//console.log(JSON.parse(JSON_string).replace(/\\"/g, '"'));
+		selected_deck_names[selected_deck_names.length] = JSON.stringify(JSON.parse(JSON_string));
 				
 	}
 	
