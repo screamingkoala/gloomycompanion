31f9837f98ee47702b3f613ee5941e2bf00d53e6
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-09-23 08:52:07 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1330,7 +1330,7 @@ function init() {
 	//First, hide all existing characters
 	total_characters=document.getElementsByName("class_name");
 	    	for(i=0; i<total_characters.length; i++){
-			document.getElementsByName("class_name")[i].style.visibility == "hidden"				
+			document.getElementsByName("class_name")[i].style.visibility == "hidden"	;			
 		}
 	    
 	    
