2a5aa1d20d97a767e3f1aca0ec56ed2de9b3e635
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-10-06 13:54:12 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1571,10 +1571,13 @@ function update_stats(){
 		for (i=0; i < cards_array.length;i++){
 
 			short_name=cards_array[i][1]
-			console.log(short_name);
 			short_name = short_name.replace(/\s/g, '');
-			console.log(short_name);			
 			short_name = short_name.substring(0, short_name.length-2);
+			if (short_name.substring(0, 1)=='<')
+			{
+			//remove everying after ">" a
+			short_name = short_name.substring(short_name.indexOf(">")+1);			
+			}
 			console.log(short_name);
 			document.getElementById(short_name).style.order = i+1;
 			
