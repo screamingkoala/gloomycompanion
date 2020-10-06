7213fc8f764f25074ed82dd18fd72323b48c3246
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-10-06 13:24:11 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1503,7 +1503,11 @@ function init() {
 			
 			if (short_name.substring(0, 1)=='<')
 			{
+			//remove everying after ">" 
+			short_name = short_name.substring(short_name.indexOf(">"), short_name.length-2);
 			short_name = short_name.substring(0, short_name.length-2);
+			console.log(short_name);
+				
 			x=document.getElementById(short_name);
 			console.log(x);
 			x=x.getElementsByClassName('name')[0].innerHTML;		
