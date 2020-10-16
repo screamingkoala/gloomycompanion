c16fc2b7b0f6db38d83ada397c50a1ac391b788f
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-10-16 12:42:02 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1573,7 +1573,7 @@ function init() {
 					}
 					else
 					{
-					document.querySelectorAll("div.card.ability.front.pull.up.discard span.initiative")[i].innerHTML = **;
+					document.querySelectorAll("div.card.ability.front.pull.up.discard span.initiative")[i].innerHTML = "**";
 					}
 				}
 
