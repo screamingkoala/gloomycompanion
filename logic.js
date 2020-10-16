9f813b8d64f042b53a4758a09c83510a9f979740
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-10-16 12:18:12 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1560,7 +1560,7 @@ function init() {
 
 
 					console.log(short_name);
-					console.log(document.getElementById(short_name).style.background);
+					console.log(document.getElementById("PC" + short_name).style.background);
 					//console.log(document.getElementById(x).style.background == "url(\"images/icon.png\") 0% 0% / 120px 120px no-repeat");
 					new_init = window.prompt("New Initiative for " + short_name);
 					if (new_init.length == 1) {
