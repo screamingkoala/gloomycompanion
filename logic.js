b5702f56824bfa4871d125a0b37609c8d805c7bf
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-10-26 13:25:32 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1169,8 +1169,7 @@ function DeckList() {
 			
 			listitem.appendChild(dom_dict.root);
 
-			var level_selector = new LevelSelector("", true);
-			level_selector.html.style = "float: right;clear: both;"
+			var level_selector = new LevelSelector("", true);			
 			listitem.appendChild(level_selector.html);
 
 
