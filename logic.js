846f2126aa970d64480fbaac1661b5fd326a0375
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-10-26 15:01:37 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1366,6 +1366,27 @@ function init() {
 		var selected_decks = selected_deck_names.map(function(deck_names) {
 			return load_ability_deck(deck_names.class, deck_names.name, deck_names.level);
 		});
+		
+		class_array = document.getElementsByName("class_name");
+		for (i = 0; i < class_array.length; i++) {
+			if (document.getElementsByName("class_name")[i].style.visibility == "visible") {
+				class_name = document.getElementsByName("class_name")[i].id;
+				class_level = document.querySelector("#" + class_name + "_level").value;
+				//replace the _ with a space 
+				class_name  = class_name.replace(/_/g, " ");
+										
+				
+				
+				class_name = class_name.charAt(0).toUpperCase() + class_name.slice(1);
+				
+				class_string = '{"name": "PC ' + class_name + '", "class": "' + class_name + '", "level": "' + class_level + '"}'
+				
+				json_string = JSON.parse(class_string);
+				
+				selected_deck_names[selected_deck_names.length] = json_string;
+			}
+		
+		
 		apply_deck_selection(selected_decks, true);
 		
 		
