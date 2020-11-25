69515fa91f385ab6a9bf3cf70484ee338ac299f5
screamingkoala <39791704+screamingkoala@users.noreply.github.com>
2020-11-25 08:44:22 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -1834,6 +1834,8 @@ function init() {
 	
 	
 	list_available_scenarios = function(){
+		available_scenarios=[];
+		
 		if (localStorage.hasOwnProperty('gloomhavenScenarioTree') == true)
 			scenario_info = JSON.parse(localStorage.getItem("gloomhavenScenarioTree"));			
 			for (i = 0; i < scenario_info.nodes.length; i++) {
